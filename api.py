8c6c7833d9d5d6e4b46f565296ec77ebc70bd091
Kade Robertson <kade@kaderobertson.pw>
2018-01-25 09:31:57 -0500

    add endpoint for emojicode

--- api.py
+++ api.py
@@ -8,19 +8,36 @@ api = Api(app)
 class UnicodeResource(Resource):
     def get(self, unicode_val):
         infodict = { 
-            'emoji_unicode': unicode_val,
+            'emojiUnicode': unicode_val,
             'emojicode': None,
-            'emojicode_alias': None 
+            'emojicodeAlias': None 
         }
         if unicode_val in emoji.UNICODE_EMOJI.keys():
             infodict['emojicode'] = emoji.UNICODE_EMOJI[unicode_val]
         else:
             return dict(), 400
         if unicode_val in emoji.UNICODE_EMOJI_ALIAS.keys():
-            infodict['emojicode_alias'] = emoji.UNICODE_EMOJI_ALIAS[unicode_val]
+            infodict['emojicodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[unicode_val]
         return infodict, 200
 
+class EmojicodeResouce(Resource):
+    def get(self, emojicode):
+        infodict = { 
+            'emojiUnicode': None,
+            'emojicode': emojicode,
+            'emojicodeAlias': None 
+        }
+        if emojicode in emoji.EMOJI_UNICODE.keys():
+            infodict['emojiUnicode'] = emoji.EMOJI_UNICODE[emojicode]
+        else:
+            return dict(), 400
+        unicode_val = infodict['emojiUnicode']
+        if unicode_val in emoji.UNICODE_EMOJI_ALIAS.keys():
+            infodict['emojicodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[unicode_val]
+        return infodict
+
 api.add_resource(UnicodeResource, '/unicode/<string:unicode_val>')
+api.add_resource(EmojicodeResouce, '/emojicode/<string:emojicode>')
 
 if __name__ == '__main__':
     app.run(debug=True)
