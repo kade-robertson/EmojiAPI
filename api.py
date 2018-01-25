2f7185d7f04576541cf5a89676c059934f424766
Kade Robertson <kade@kaderobertson.pw>
2018-01-25 09:45:57 -0500

    add endpoint for emojicode alias, ignore bad data

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import emoji
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -14,11 +14,9 @@ class UnicodeResource(Resource):
         }
         if unicode_val in emoji.UNICODE_EMOJI.keys():
             infodict['emojicode'] = emoji.UNICODE_EMOJI[unicode_val]
-        else:
-            return dict(), 400
         if unicode_val in emoji.UNICODE_EMOJI_ALIAS.keys():
             infodict['emojicodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[unicode_val]
-        return infodict, 200
+        return infodict
 
 class EmojicodeResouce(Resource):
     def get(self, emojicode):
@@ -29,15 +27,26 @@ class EmojicodeResouce(Resource):
         }
         if emojicode in emoji.EMOJI_UNICODE.keys():
             infodict['emojiUnicode'] = emoji.EMOJI_UNICODE[emojicode]
-        else:
-            return dict(), 400
-        unicode_val = infodict['emojiUnicode']
-        if unicode_val in emoji.UNICODE_EMOJI_ALIAS.keys():
-            infodict['emojicodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[unicode_val]
+        if infodict['emojiUnicode'] is not None:
+            infodict['emojicodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[infodict['emojiUnicode']]
+        return infodict
+
+class EmojicodeAliasResouce(Resource):
+    def get(self, alias):
+        infodict = { 
+            'emojiUnicode': None,
+            'emojicode': None,
+            'emojicodeAlias': alias 
+        }
+        if alias in emoji.EMOJI_ALIAS_UNICODE.keys():
+            infodict['emojiUnicode'] = emoji.EMOJI_ALIAS_UNICODE[alias]
+        if infodict['emojiUnicode'] is not None:
+            infodict['emojicode'] = emoji.UNICODE_EMOJI[infodict['emojiUnicode']]
         return infodict
 
 api.add_resource(UnicodeResource, '/unicode/<string:unicode_val>')
 api.add_resource(EmojicodeResouce, '/emojicode/<string:emojicode>')
+api.add_resource(EmojicodeAliasResouce, '/alias/<string:alias>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
