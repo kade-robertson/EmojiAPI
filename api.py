d8a5451992d1d897c9a10a10b4efa839ff1dc5d1
Kade Robertson <kade@kaderobertson.pw>
2018-01-25 10:05:33 -0500

    match Google JSON convention

--- api.py
+++ api.py
@@ -9,39 +9,39 @@ class UnicodeResource(Resource):
     def get(self, unicode_val):
         infodict = { 
             'emojiUnicode': unicode_val,
-            'emojicode': None,
-            'emojicodeAlias': None 
+            'emojiCode': None,
+            'emojiCodeAlias': None 
         }
         if unicode_val in emoji.UNICODE_EMOJI.keys():
-            infodict['emojicode'] = emoji.UNICODE_EMOJI[unicode_val]
+            infodict['emojiCode'] = emoji.UNICODE_EMOJI[unicode_val]
         if unicode_val in emoji.UNICODE_EMOJI_ALIAS.keys():
-            infodict['emojicodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[unicode_val]
+            infodict['emojiCodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[unicode_val]
         return infodict
 
 class EmojicodeResouce(Resource):
     def get(self, emojicode):
         infodict = { 
             'emojiUnicode': None,
-            'emojicode': emojicode,
-            'emojicodeAlias': None 
+            'emojiCode': emojicode,
+            'emojiCodeAlias': None 
         }
         if emojicode in emoji.EMOJI_UNICODE.keys():
             infodict['emojiUnicode'] = emoji.EMOJI_UNICODE[emojicode]
         if infodict['emojiUnicode'] is not None:
-            infodict['emojicodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[infodict['emojiUnicode']]
+            infodict['emojiCodeAlias'] = emoji.UNICODE_EMOJI_ALIAS[infodict['emojiUnicode']]
         return infodict
 
 class EmojicodeAliasResouce(Resource):
     def get(self, alias):
         infodict = { 
             'emojiUnicode': None,
-            'emojicode': None,
-            'emojicodeAlias': alias 
+            'emojiCode': None,
+            'emojiCodeAlias': alias 
         }
         if alias in emoji.EMOJI_ALIAS_UNICODE.keys():
             infodict['emojiUnicode'] = emoji.EMOJI_ALIAS_UNICODE[alias]
         if infodict['emojiUnicode'] is not None:
-            infodict['emojicode'] = emoji.UNICODE_EMOJI[infodict['emojiUnicode']]
+            infodict['emojiCode'] = emoji.UNICODE_EMOJI[infodict['emojiUnicode']]
         return infodict
 
 api.add_resource(UnicodeResource, '/unicode/<string:unicode_val>')
